ce8552687fe289e8d106de08476d81516174edbb
Nahuel-Troisi <nahueltroisi7291@gmail.com>
2023-04-24 12:32:34 +0000

    Añadimos el archivo "main"

---
+++ Scripts/main.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+
+# Rutas de los scripts
+
+script_paths = {
+
+    "MySQL": "/home/whippy/Scripts/Tools/mysql_config.py",
+
+    "Backup": "/home/whippy/Scripts/Tools/backup.py",
+
+    "Scan Ports": "/home/whippy/Scripts/Tools/scan_config.py",
+
+    "Passwd": "/home/whippy/Scripts/Tools/create_passwd.py",
+
+    "Create Users (Linux)": "/home/whippy/Scripts/Tools/create_users.py",
+
+    "Delete Users (Linux)": "/home/whippy/Scripts/Tools/delete_users.py",
+
+    "Create Users (Windows)": "/home/whippy/Scripts/Tools/create_users.ps1",
+
+    "Delete Users (Windows)": "/home/whippy/Scripts/Tools/delete_users.ps1",
+
+}
+
+
+
+# Función para ejecutar un script
+
+def ejecutar_script(ruta_script):
+
+    if ruta_script.endswith(".py"):
+
+        subprocess.run(["python3", ruta_script])
+
+    elif ruta_script.endswith(".ps1"):
+
+        subprocess.run(["powershell.exe", "-File", ruta_script])
+
+    else:
+
+        print("Tipo de archivo no compatible:", ruta_script)
+
+
+
+# Menú interactivo para seleccionar un script
+
+while True:
+
+    print("Seleccione el script que desea ejecutar:")
+
+    for i, script_name in enumerate(script_paths.keys()):
+
+        print(f"{i+1}. {script_name}")
+
+
+    opcion = input("Ingrese el número del script que desea ejecutar (o 'q' para salir): ")
+
+    if opcion.lower() == "q":
+
+        break
+
+    try:
+
+        opcion = int(opcion)
+
+        script_name = list(script_paths.keys())[opcion-1]
+
+        ruta_script = script_paths[script_name]
+
+        ejecutar_script(ruta_script)
+
+
+
+    except (ValueError, IndexError):
+
+        print("Opción inválida. Por favor ingrese un número del menú.")
+
